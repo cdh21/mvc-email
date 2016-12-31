0d5bc45c982ebbe1175271992229988b28678744
Lionuser <9370ssc@hanmail.net>
2016-12-31 02:29:28 +0000

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,6 +4,18 @@ class EmailController < ApplicationController
 
 	def email_send
 		@email = params[:email]
+		title = params[:title]
 		@content = params[:content]
+
+		gmail = Gmail.connect("이메일주소", "비밀번호")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
 	end
 end
